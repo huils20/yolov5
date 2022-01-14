0e96da5f77f7def64021b1aeaec995de48399bec
huils20 <huils20@qq.com>
2022-01-14 23:14:55 +0800

    0114

--- models/mycommon3.py
+++ models/mycommon3.py
@@ -140,7 +140,7 @@ class C3_6(nn.Module):
     def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
         super().__init__()
         c_ = int(c2 * e)  # hidden channels
-        self.cv1 = (c1, c_, 1, 1)
+        self.cv1 = Conv(c1, c_, 1, 1)
         self.cv2 = Conv(c1, c_, 1, 1)
         self.cv3 = Conv(2 * c_, c2, 1)  # act=FReLU(c2)
         self.m = nn.Sequential(*(Bottleneck(c_, c_, shortcut, g, e=1.0) for _ in range(n)))
